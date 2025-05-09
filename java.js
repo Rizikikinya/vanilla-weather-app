90c36a73c27bdba88f1b2ed695b37fc57534813d
kinyalinah <kinyalinah6@gmail.com>
2025-05-09 14:48:46 +0300

    updated search ingine

--- java.js
+++ java.js
@@ -4,10 +4,13 @@ function searchCityEngine(response) {
   let temperature = Math.round(response.data.temperature.current);
   let temperatureValue = document.querySelector("#temperature-value");
   temperatureValue.innerHTML = `${temperature}`;
-  let apiKey = "6fe8tf4ae3fc290f9f3ff43213b0b7od";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityNameElement}&key=${apiKey}&unit=metric`;
-
-  axios.get(apiUrl).then(searchCityEngine);
+  let humidity = response.data.temperature.humidity;
+  let humidityValue = document.querySelector("#humidity-value");
+  humidityValue.innerHTML = `${humidity}`;
+  let cloudcondition = document.querySelector("#cloud-condition");
+  cloudcondition.innerHTML = response.data.condition.description;
+  let wind = document.querySelector("#wind-value");
+  wind.innerHTML = response.data.wind.speed;
 }
 
 function searchCity(event) {
@@ -15,6 +18,9 @@ function searchCity(event) {
   let input = document.querySelector("#searchform-input");
   let cityName = document.querySelector("#city-name");
   cityName.innerHTML = input.value;
+  let apiKey = "6fe8tf4ae3fc290f9f3ff43213b0b7od";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${input.value}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(searchCityEngine);
 }
 
 let form = document.querySelector("#search-form");
