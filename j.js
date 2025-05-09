3021a6696af78eded4b378d6aa0494a6d7797c39
kinyalinah <kinyalinah6@gmail.com>
2025-05-09 13:58:39 +0300

    UPDATED JAVA

--- j.js
+++ j.js
@@ -1,12 +1,13 @@
 function searchCityEngine(response) {
-  let cityNameElement = document.querySelector("#searchform-input");
-  cityNameElement.innerHTML = response.data.city;
   let temperature = Math.round(response.data.temperature.current);
   let temperatureValue = document.querySelector("#temperature-value");
   temperatureValue.innerHTML = `${temperature}`;
-  let apiKey = "6fe8tf4ae3fc290f9f3ff43213b0b7od";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityNameElement}&key=${apiKey}&unit=metric`;
+}
 
+function displayCity(input) {
+  let apiKey = "6fe8tf4ae3fc290f9f3ff43213b0b7od";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}&unit=metric`;
+  apiUrl = apiUrl.replace("{query}", input.value).replace("{key}", apiKey);
   axios.get(apiUrl).then(searchCityEngine);
 }
 
@@ -15,6 +16,8 @@ function searchCity(event) {
   let input = document.querySelector("#searchform-input");
   let cityName = document.querySelector("#city-name");
   cityName.innerHTML = input.value;
+
+  displayCity(input); // Call the function to display the city
 }
 
 let form = document.querySelector("#search-form");
